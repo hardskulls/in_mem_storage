216d5f35ebe749461d228f9b33f02fed72815f24
hardskulls <hardskulls@rambler.ru>
2023-10-16 09:34:54 +0300

    dirty(application/service/rate_limiter): Fixed generic types on `RateLimiter`.

--- application/service/rate_limiter/rate_limiter.go
+++ application/service/rate_limiter/rate_limiter.go
@@ -5,23 +5,23 @@ import (
 	lim "in_mem_storage/domain/rate_limiter/value_object"
 )
 
-type RateLimitService[U, L any, K comparable] struct {
-	rateLimitRepo repo.RateLimitRepo[U, L, K]
+type RateLimitService[K comparable, U, L any] struct {
+	rateLimitRepo repo.RateLimitRepo[K, U, L]
 }
 
-func WithRateLimitRepo[U, L any, K comparable](repo repo.RateLimitRepo[U, L, K]) RateLimitService[U, L, K] {
-	return RateLimitService[U, L, K]{rateLimitRepo: repo}
+func WithRateLimitRepo[U, L any, K comparable](repo repo.RateLimitRepo[K, U, L]) RateLimitService[K, U, L] {
+	return RateLimitService[K, U, L]{rateLimitRepo: repo}
 }
 
-func (rl *RateLimitService[U, L, K]) Get(key K) (lim.RateLimit[U, L], error) {
+func (rl *RateLimitService[K, U, L]) Get(key K) (lim.RateLimit[U, L], error) {
 	return rl.rateLimitRepo.Get(key)
 }
 
-func (rl *RateLimitService[U, L, K]) Set(key K, value lim.RateLimit[U, L]) error {
+func (rl *RateLimitService[K, U, L]) Set(key K, value lim.RateLimit[U, L]) error {
 	return rl.rateLimitRepo.Set(key, value)
 }
 
-func (rl *RateLimitService[U, L, K]) Delete(key K) error {
+func (rl *RateLimitService[K, U, L]) Delete(key K) error {
 	return rl.rateLimitRepo.Delete(key)
 }
 
